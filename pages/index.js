86a4875503cbb15b9e6f9780a6d77424eaada74a
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-09-11 16:27:07 -0300

    Add audio and captions

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,125 @@
-import React from "react";
+import React, { Children } from "react";
+import { MDXProvider } from "@mdx-js/react";
 import { MiniBrowser } from "@code-hike/mini-browser";
 import { MiniEditor } from "@code-hike/mini-editor";
+import { Video } from "@code-hike/player";
 import { useSpring } from "use-spring";
+import Steps from "../steps.mdx";
 
 export default function Page() {
+  return (
+    <MDXProvider components={components}>
+      <Steps />
+    </MDXProvider>
+  );
+}
+
+const components = {
+  wrapper: Wrapper,
+};
+
+function Wrapper({ children }) {
+  const { videoSteps, browserSteps, codeSteps, captionSteps } = getStepsFromMDX(
+    children
+  );
+  return (
+    <Screencast
+      videoSteps={videoSteps}
+      browserSteps={browserSteps}
+      codeSteps={codeSteps}
+      captionSteps={captionSteps}
+    />
+  );
+}
+
+function getStepsFromMDX(children) {
+  const splits = [[]];
+
+  Children.forEach(children, (child) => {
+    if (child.props.mdxType === "hr") {
+      splits.push([]);
+    } else {
+      const lastSplit = splits[splits.length - 1];
+      lastSplit.push(child);
+    }
+  });
+
+  const videoSteps = splits.map((split) => {
+    const videoElement = split.find((child) => child.props.mdxType === "Video");
+    return videoElement.props;
+  });
+
+  const browserSteps = splits.map((split) => {
+    const browserElement = split.find(
+      (child) => child.props.mdxType === "Browser"
+    );
+    const { children, ...rest } = browserElement.props;
+    const actions = Children.map(children, (child) => child.props);
+    return {
+      actions,
+      ...rest,
+    };
+  });
+
+  const codeSteps = splits.map((split) => {
+    const editorElement = split.find(
+      (child) => child.props.mdxType === "Editor"
+    );
+    const { code, ...rest } = editorElement.props;
+    return {
+      code: require(`!!raw-loader!../public/${code}.html`).default,
+      // file: tab,
+      ...rest,
+    };
+  });
+
+  const captionSteps = splits.map((split) => {
+    const pre = split.find(
+      (child) =>
+        child.props.mdxType === "pre" &&
+        child.props.children.props.className === "language-srt"
+    );
+    if (!pre) return [];
+
+    return parseSrt(pre.props.children.props.children);
+  });
+
+  return {
+    videoSteps,
+    browserSteps,
+    codeSteps,
+    captionSteps,
+  };
+}
+
+function parseSrt(srt) {
+  const regex = /^[\d\.\:]+\s+[–\-]>\s+[\d\.\:]+$/gm;
+  const times = srt.match(regex);
+  if (!times) return [];
+  const [, ...texts] = srt.split(regex);
+  return times.map((time, i) => {
+    const [start, end] = time.match(/[\d\.]+/g).map((t) => +t);
+    return { start, end, text: texts[i].trim() };
+  });
+}
+
+function Screencast({ videoSteps, browserSteps, codeSteps, captionSteps }) {
   const [step, setStep] = React.useState(0);
+  const [videoTime, setVideoTime] = React.useState(videoSteps[0].start);
   const [progress] = useSpring(step, {
     decimals: 3,
     stiffness: 80,
     damping: 48,
     mass: 8,
   });
+  const playerRef = React.useRef();
+
+  const caption = useCaption(captionSteps, step, videoTime);
+
+  const onTimeChange = (newTime, oldTime) => {
+    setVideoTime(newTime);
+  };
+
   return (
     <div
       style={{
@@ -19,6 +128,7 @@ export default function Page() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexFlow: "column",
       }}
     >
       <style jsx global>{`
@@ -42,6 +152,7 @@ export default function Page() {
           style={{ gridArea: "1 / 1 / 6 / 4" }}
           steps={codeSteps}
           lang="html"
+          file="index.html"
           progress={progress}
         />
         <MiniBrowser
@@ -61,64 +172,45 @@ export default function Page() {
             height: "100%",
             boxShadow:
               "0 6px 12px -2px rgba(50, 50, 93, 0.25), 0 3px 7px -3px rgba(0, 0, 0, 0.3)",
+            whiteSpace: "pre",
+            color: "#ddd",
+            alignItems: "center",
+            justifyContent: "center",
+            display: "flex",
+            fontSize: "1.4em",
+            lineHeight: "1.4em",
+            textAlign: "center",
           }}
-          onClick={() => setStep(step + 1)}
-        ></div>
+        >
+          {caption}
+        </div>
       </main>
+
+      <div style={{ marginTop: 48 }}>
+        <button onClick={() => playerRef.current.play()}>Play</button>
+        <Video
+          steps={videoSteps}
+          containerStyle={{
+            height: "100px",
+            display: "none",
+          }}
+          onStepChange={setStep}
+          onTimeChange={onTimeChange}
+          ref={playerRef}
+        />
+      </div>
     </div>
   );
 }
 
-const browserSteps = [
-  { loadUrl: "/1.html" },
-  { loadUrl: "/2.html" },
-  { loadUrl: "/3.html" },
-  { loadUrl: "/3.1.html" },
-  { loadUrl: "/4.html" },
-  { loadUrl: "/5.html" },
-  // { loadUrl: "/5.html" },
-  { loadUrl: "/6.html" },
-];
+function useCaption(captionSteps, stepIndex, videoTime) {
+  const stepCaptions = captionSteps[stepIndex];
 
-const codeSteps = [
-  {
-    code: require(`!!raw-loader!../public/1.html`).default,
-    file: "index.html",
-    focus: "1:2,11:13",
-  },
-  {
-    code: require(`!!raw-loader!../public/2.html`).default,
-    file: "index.html",
-    focus: "10:12,14",
-  },
-  {
-    code: require(`!!raw-loader!../public/3.html`).default,
-    file: "index.html",
-    focus: "10:14,16",
-  },
-  {
-    code: require(`!!raw-loader!../public/3.1.html`).default,
-    file: "index.html",
-    focus: "12:22",
-  },
-  {
-    code: require(`!!raw-loader!../public/4.html`).default,
-    file: "index.html",
-    focus: "10:12",
-  },
-  {
-    code: require(`!!raw-loader!../public/5.html`).default,
-    file: "index.html",
-    focus: "10:12,14:18",
-  },
-  // {
-  //   code: require(`!!raw-loader!../public/5.html`).default,
-  //   file: "index.html",
-  //   focus: "11[17:34],15[15:31]",
-  // },
-  {
-    code: require(`!!raw-loader!../public/6.html`).default,
-    file: "index.html",
-    focus: "10:12,14:26",
-  },
-];
+  if (!stepCaptions) return null;
+
+  const caption = stepCaptions.find(
+    ({ start, end }) => start <= videoTime && videoTime < end
+  );
+
+  return caption ? caption.text : null;
+}
---
+++ transcript.md
@@ -0,0 +1,95 @@
+We can apply several of graphical effects to any HTML element using the filter CSS property.
+
+---
+
+For example, we can blur an h1 using the blur function.
+
+---
+
+Or, add a shadow using the drop-shadow function.
+
+---
+
+Here are the ten different functions that you can use and combine.
+
+---
+
+But there's one additional function, which by itself doesn't do anything.
+
+It takes an id as an argument.
+
+---
+
+And too apply the effect we need an SVG filter with that id.
+
+SVG filters are much more powerful than the CSS filters, but they are also more complicated.
+
+---
+
+There are many primitives that you can compose to make advanced effects.
+
+Here's an example combining two of those primitives.
+
+---
+
+<Video src="1.mp4" start={0} end={8} />
+<Browser loadUrl="/1.html" />
+<Editor code="1" />
+
+```srt
+0 -> 9
+We can apply several graphical
+effects to any HTML element
+using the filter CSS property
+```
+
+---
+
+<Video src="2.mp4" start={0} end={5} />
+<Browser loadUrl="/2.html" />
+<Editor code="2" />
+
+```srt
+0 -> 7
+For example, we can blur an h1
+using the blur function
+```
+
+---
+
+<Video src="3.mp4" start={0} end={4} />
+<Browser loadUrl="/3.html" />
+<Editor code="3" />
+
+```srt
+0 -> 5
+Or, add a shadow using drop-shadow
+```
+
+---
+
+<Video src="3.1.mp4" start={0} end={6} />
+<Browser loadUrl="/3.1.html" />
+<Editor code="3.1" />
+
+```srt
+0 -> 8
+Here are the ten different functions
+that you can use and combine
+```
+
+---
+
+<Video src="4.mp4" start={0} end={9} />
+<Browser loadUrl="/4.html" />
+<Editor code="4" />
+
+```srt
+0 -> 6
+But there's one additional function,
+which by itself doesn't do anything
+6 -> 10
+It takes an id as an argument
+```
+
+---
